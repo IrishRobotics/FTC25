363b025fb6d0743e35ab85620facf968fc77321b
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-10-21 18:40:43 -0500

    Fix my inversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NicksFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NicksFIRSTJavaOpMode.java
@@ -28,9 +28,9 @@ public class NicksFIRSTJavaOpMode extends OpMode {
 
     @Override
     public void loop() {
-        double y = gamepad1.left_stick_y;
-        double x = -gamepad1.left_stick_x;
-        double rx = -gamepad1.right_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
 
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontLeftPower = (y + x + rx) / denominator;
