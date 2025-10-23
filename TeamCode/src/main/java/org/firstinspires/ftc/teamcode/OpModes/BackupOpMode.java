2afae90665d1a92caee87816e7ebe698eaa391bc
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-10-23 08:02:21 -0500

    Add backup op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BackupOpMode.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.RunShooter;
+import org.firstinspires.ftc.teamcode.commands.UserDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+
+@TeleOp(name = "BackupOpMode", group = "NormalOpModes")
+public class BackupOpMode extends CommandOpMode {
+    private Shooter shooter;
+    private Drivetrain drivetrain;
+
+    private GamepadEx driverOp;
+    private GamepadEx coOp;
+
+    @Override
+    public void initialize() {
+        driverOp = new GamepadEx(gamepad1);
+        coOp = new GamepadEx(gamepad2);
+
+        drivetrain = new Drivetrain(hardwareMap, "frontLeftDrive", "frontRightDrive", "backLeftDrive", "backRightDrive");
+        drivetrain.setDefaultCommand(new UserDrive(drivetrain, driverOp));
+
+        shooter = new Shooter(hardwareMap, "shooterMotor");
+        coOp.getGamepadButton(GamepadKeys.Button.A).whileHeld(new RunShooter(shooter, 1));
+    }
+}
