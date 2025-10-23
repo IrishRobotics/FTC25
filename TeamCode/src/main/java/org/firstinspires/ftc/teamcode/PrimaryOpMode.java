f7fea788ccb59262ac2f07873b536fe851ba2e65
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-10-22 19:49:29 -0500

    Created the UserDrive command and the Drivetrain subsystem. Added the new subsystem and command. Fixed PrimaryOpMode to be proper type.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrimaryOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrimaryOpMode.java
@@ -1,47 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.commands.RunShooter;
+import org.firstinspires.ftc.teamcode.commands.UserDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
 
 @TeleOp(name = "PrimaryOpMode", group= "NormalOpModes")
-public class PrimaryOpMode extends OpMode {
-    private Motor fL, fR, bL, bR;
-    private MecanumDrive drive;
+public class PrimaryOpMode extends CommandOpMode {
     private Shooter shooter;
-    private GamepadEx driverOp;
+    private Drivetrain drivetrain;
 
+    private GamepadEx driverOp;
     private GamepadEx coOp;
 
     @Override
-    public void init() {
-        fL = new Motor(hardwareMap, "frontLeftDrive");
-        fR = new Motor(hardwareMap, "frontRightDrive");
-        bL = new Motor(hardwareMap, "backLeftDrive");
-        bR = new Motor(hardwareMap, "backRightDrive");
-
-        drive = new MecanumDrive(fL, fR, bL, bR);
+    public void initialize() {
         driverOp = new GamepadEx(gamepad1);
         coOp = new GamepadEx(gamepad2);
 
+        drivetrain = new Drivetrain(hardwareMap, "frontLeftDrive", "frontRightDrive", "backLeftDrive", "backRightDrive");
+        drivetrain.setDefaultCommand(new UserDrive(drivetrain, driverOp));
+
         shooter = new Shooter(hardwareMap, "shooterMotor");
-
         coOp.getGamepadButton(GamepadKeys.Button.A).whileHeld(new RunShooter(shooter, 1));
-    }
-
-    @Override
-    public void loop() {
-        drive.driveRobotCentric(
-                driverOp.getLeftX(),
-                driverOp.getLeftY(),
-                driverOp.getRightX()
-        );
     }
 }
