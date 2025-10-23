cc4a38eb5ef1abf6792c5ccfd2081d6c419e4230
Nick <ngarnsworthy@gmail.com>
2025-10-23 18:37:44 -0500

    Added Intake Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/RunIntake.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+
+public class RunIntake extends CommandBase {
+
+    private Intake subsystem;
+
+    private double speed;
+
+    public RunIntake(Intake _subsystem, double _speed) {
+        subsystem = _subsystem;
+        addRequirements(subsystem);
+        speed = _speed;
+    }
+
+    @Override
+    public void initialize() {
+        subsystem.run(speed);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        subsystem.stop();
+    }
+}
