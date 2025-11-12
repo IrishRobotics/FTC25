c4e7cabcceee92735cd9914f61826be74725d789
Nick <ngarnsworthy@gmail.com>
2025-11-11 18:40:53 -0600

    Added New Ramp Motor. Started Work on New Speed Functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -5,25 +5,29 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Shooter extends SubsystemBase {
-    private final Motor shooterMotor, rampMotor;
+    private final Motor shooterMotor, rampMotorOne, rampMotorTwo;
 
-    public Shooter(HardwareMap map, String shooterMotorName, String rampMotorName) {
+    public Shooter(HardwareMap map, String shooterMotorName, String rampMotorOneName, String rampMotorTwoName) {
         shooterMotor = new Motor(map, shooterMotorName);
         shooterMotor.setRunMode(Motor.RunMode.RawPower);
-        rampMotor = new Motor(map, rampMotorName);
-        rampMotor.setRunMode(Motor.RunMode.RawPower);
+        rampMotorOne = new Motor(map, rampMotorOneName);
+        rampMotorOne.setRunMode(Motor.RunMode.RawPower);
+        rampMotorTwo = new Motor(map, rampMotorTwoName);
+        rampMotorTwo.setRunMode(Motor.RunMode.RawPower);
     }
 
-    public void run(double shooterSpeed, double rampSpeed) {
+    public void runBase(double shooterSpeed, double rampSpeedOne) {
         // Clamp speed to valid motor range [-1.0, 1.0]
-        double clampedSpeed = Math.max(-1.0, Math.min(1.0, shooterSpeed));
-        double _rampSpeed = Math.max(-1.0, Math.min(1.0, rampSpeed));
-        shooterMotor.set(clampedSpeed);
-        rampMotor.set(_rampSpeed);
+        double baseSpeed = Math.max(-1.0, Math.min(1.0, shooterSpeed));
+        double _rampSpeed = Math.max(-0.5, Math.min(0.5, rampSpeedOne));
+        shooterMotor.set(baseSpeed);
+        rampMotorOne.set(_rampSpeed);
+        rampMotorTwo.set(_rampSpeed * -1.0);
     }
 
     public void stop() {
         shooterMotor.stopMotor();
-        rampMotor.stopMotor();
+        rampMotorOne.stopMotor();
+        rampMotorTwo.stopMotor();
     }
 }
