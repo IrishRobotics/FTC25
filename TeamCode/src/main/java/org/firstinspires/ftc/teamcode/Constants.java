e807b5cbca5ef6e8372fd7069828d97aa3d6b5f5
Nicholas Garnsworthy <ngarnsworthy@gmail.com>
2025-11-11 19:48:30 -0600

    Created odometry for Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,11 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+
 public final class Constants {
     public static class Drivetrain {
         public static final String flMotorName = "frontLeftDrive";
         public static final String frMotorName = "frontRightDrive";
         public static final String blMotorName = "backLeftDrive";
         public static final String brMotorName = "backRightDrive";
+
+        private static final double length = 0.2873375;
+        private static final  double width = 0.3444875;
+        public static final Translation2d frontLeftLocation =
+                new Translation2d(length/2, 0.381);
+        public static final Translation2d frontRightLocation =
+                new Translation2d(length/2, -0.381);
+        public static final Translation2d backLeftLocation =
+                new Translation2d(-length/2, 0.381);
+        public static final Translation2d backRightLocation =
+                new Translation2d(-length/2, -0.381);
+
+        public static final MecanumDriveKinematics kinematics = new MecanumDriveKinematics
+                (
+                        frontLeftLocation, frontRightLocation,
+                        backLeftLocation, backRightLocation
+                );
     }
 
     public static class Shooter {
